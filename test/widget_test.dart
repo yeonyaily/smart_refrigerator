f992f3ff1ab23c248d307113be0477caa83a7a2f
yeonyaily <yeonye330@gmail.com>
2021-05-17 22:03:17 +0900

    create login page

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:smart_refrigerator/main.dart';
+import 'package:smart_refrigerator/start.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(Start());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
