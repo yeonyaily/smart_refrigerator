f8a52fdce64730deb2a1399a2f10f6e2cf887c7d
Turbstructor <ycw0201@naver.com>
2021-06-07 01:38:45 +0900

    added nearby_search

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyBk9KTKPjv2s5uUnNJLTVvIzIYUCJ3-Qik")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
